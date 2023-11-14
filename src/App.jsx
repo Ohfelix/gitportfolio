8be4098c059d67508c1224c4c0826aba58b2e333
Leandro Felix <leandroofelix90@gmail.com>
2023-11-14 10:54:57 -0300

    app atz4

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ export default function App() {
     <>
       <div className="p-4 m-5">
         <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-          <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/sarah-dayan.jpg" alt="" width="384" height="512" />
+         {/*  <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/sarah-dayan.jpg" alt="" width="384" height="512" /> */}
           <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
             <blockquote>
               <p className="text-lg font-medium">
@@ -13,7 +13,7 @@ export default function App() {
               </p>
             </blockquote>
             <figcaption className="font-medium">
-              <div class="text-sky-500 dark:text-sky-400">
+              <div className="text-sky-500 dark:text-sky-400">
                 Sarah Dayan
               </div>
               <div className="text-slate-700 dark:text-slate-500">
